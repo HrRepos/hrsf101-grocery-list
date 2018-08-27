9e50f880f5d20606d2d284a6c33b074a2cf34fa8
Beth Johnson <bethjohnson3@gmail.com>
2018-08-27 11:22:18 -0700

    add schema for grocery_list db and groceries table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
-
+-- create database
+CREATE DATABASE grocery_list;
+-- use database
+USE grocery_list;
+-- create table
+CREATE TABLE IF NOT EXISTS groceries (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  quantity INT NOT NULL
+);
+-- insert sample item
+INSERT INTO groceries VALUES (1, "pizza", 5);
